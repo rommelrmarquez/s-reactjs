db9209cebb2b8f54e8ba83251113524ca5432b03
U-OVERRIDDEN\Rommel <rommelrmarquez@gmail.com>
2020-11-03 13:37:37 +0800

    transform to functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 
-class Dish extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+function RenderDish({ dish }) {
+    if (dish != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        );
     }
+}
 
-    renderComments(comments) {
-        const coms = comments.map((comment) => {
-            const date = new Date(comment.date)
-            const commentDate =
-                date.toLocaleString('en-us', {'month': 'short'}) + " " +
-                date.toLocaleString('en-us', {'day': '2-digit'}) + ", " +
-                date.toLocaleString('en-us', {'year': 'numeric'})
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p> -- {comment.author}, {commentDate}</p>
-                </li>
-            )
-        })
-        if (comments != null) {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        { coms }
-                    </ul>
-                </div>
-            )
-        } else {
-            return (<div></div>)
-        }
+function RenderComments({comments}) {
+    const coms = comments.map((comment) => {
+        const date = new Date(comment.date)
+        const commentDate =
+            date.toLocaleString('en-us', {'month': 'short'}) + " " +
+            date.toLocaleString('en-us', {'day': '2-digit'}) + ", " +
+            date.toLocaleString('en-us', {'year': 'numeric'})
+        return (
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p> -- {comment.author}, {commentDate}</p>
+            </li>
+        )
+    })
+    if (comments != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    { coms }
+                </ul>
+            </div>
+        )
+    } else {
+        return (<div></div>)
     }
+}
 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg src={dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        } else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-    render() {
-        const dish = this.props.dish
+const Dish = (props) => {
+    const dish = props.dish
         if (dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(dish)}
-                        {this.renderComments(dish.comments)}
+                        <RenderDish dish={dish}/>
+                        <RenderComments comments={dish.comments}/>
                     </div>
                 </div>
             )
@@ -72,7 +66,7 @@ class Dish extends Component {
                 <div></div>
             );
         }
-    }
 }
+
 
 export default Dish;
